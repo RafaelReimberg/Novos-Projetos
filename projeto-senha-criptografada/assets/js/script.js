d2a1f1444dc085d5853f4aff73b54affa46b5628
rafaelreimerbg <raaafael.reimberg@hotmail.com>
2023-09-15 10:11:59 -0300

    Atualização do gerador de senha criptografada

--- projeto-senha-criptografada/assets/js/script.js
+++ projeto-senha-criptografada/assets/js/script.js
@@ -1,33 +1,33 @@
-function gerarSenhaCriptografada() {
-    const senhaInput = document.getElementById('password');
-    const senha = senhaInput.value;
+    function gerarSenhaCriptografada() {
+        const senhaInput = document.getElementById('password');
+        const senha = senhaInput.value;
 
-    // Verificar se a senha não está vazia
-    if (senha.trim() === '') {
-        alert('Por favor, insira uma senha válida.');
-        return;
+        // Verificar se a senha não está vazia
+        if (senha.trim() === '') {
+            alert('Por favor, insira uma senha válida.');
+            return;
+        }
+
+        // Criptografar a senha usando SHA-256 (como no exemplo anterior)
+        sha256(senha)
+            .then(senhaCriptografada => {
+                const senhaCriptografadaElement = document.getElementById('senhaCriptografada');
+                senhaCriptografadaElement.textContent = senhaCriptografada;
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
 
-    // Criptografar a senha usando SHA-256 (como no exemplo anterior)
-    sha256(senha)
-        .then(senhaCriptografada => {
-            const senhaCriptografadaElement = document.getElementById('senhaCriptografada');
-            senhaCriptografadaElement.textContent = senhaCriptografada;
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
-
-function sha256(str) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(str);
-    return crypto.subtle.digest('SHA-256', data)
-        .then(buffer => {
-            const array = Array.from(new Uint8Array(buffer));
-            return array.map(byte => byte.toString(16).padStart(2, '0')).join('');
-        });
-}
+    function sha256(str) {
+        const encoder = new TextEncoder();
+        const data = encoder.encode(str);
+        return crypto.subtle.digest('SHA-256', data)
+            .then(buffer => {
+                const array = Array.from(new Uint8Array(buffer));
+                return array.map(byte => byte.toString(16).padStart(2, '0')).join('');
+            });
+    }
 
     const pass = document.getElementById('password');
     const icon = document.getElementById('icon');
@@ -41,4 +41,4 @@ function sha256(str) {
             pass.setAttribute('type', 'password');
             icon.classList.remove('hide')
         }
-    };      
+    }      
